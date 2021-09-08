dcd288fd946cf326e7ce0c7249a7eca1bfdf6643
Sergey Mazovets <smazovets@gmail.com>
2021-09-09 00:08:41 +0700

    Второй

--- src/main/java/HomeWorkApp.java
+++ src/main/java/HomeWorkApp.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class HomeWorkApp {
+public class HomeWorkApp {
+
+  public static void main(String[] args){
+    printThreeWords();
+  }
+
+  private static void printThreeWords(){
+    System.out.println("Orange");
+    System.out.println("Banana");
+    System.out.println("Apple");
+  }
+
 }
